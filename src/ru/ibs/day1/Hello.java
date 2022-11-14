9fa9271283a3ce2d6714076db381477d216be7f7
venera <venera.to@zohomail.com>
2022-11-14 22:34:07 +0300

    + Utils

--- src/ru/ibs/day1/Hello.java
+++ src/ru/ibs/day1/Hello.java
@@ -1,7 +1,10 @@
 package ru.ibs.day1;
+import ru.ibs.day1.util.Utils;
+import static ru.ibs.day1.util.Utils.print;
+
 public class Hello {
    public static void main(String[] args) {
-       String message = "Hello, World!";
-       System.out.println(message);
+       String message = Utils.getMessage();
+       print(message);
    }
 }
---
+++ src/ru/ibs/day1/util/Utils.java
@@ -0,0 +1,11 @@
+package ru.ibs.day1.util;
+
+public class Utils {
+    public static String getMessage(){
+        return "Hello, World!";
+    }
+
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+}
